63e71df28e5402ba15813b18a56ac0d9111f71fd
Abhishek Butola <abhishekbutola08@gmail.com>
2020-06-17 08:50:19 +0530

    initial

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5004;
+
+app.get('/', (req, res) => {
+  const html = `
+        <html> 
+            <head> 
+                <title> 
+                    UU Demo
+                </title>
+            </head>
+            <body>
+                <h1>Uttaranchal University </h1>
+                <ul> 
+                 <li>Project 1
+                 <li> Project 2
+                 <li> project 3
+                </ul>
+            </body>
+        </html>
+
+    `;
+
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log('Server running');
+});
